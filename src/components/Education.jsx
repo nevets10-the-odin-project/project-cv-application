bb62d4f3a895676307d6f595bef5bfd5e67ffa96
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-21 08:25:55 -0700

    Fill out fields for Education and Professional

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useState } from "react";
+import Form from "./Form";
+
+const initialFields = [
+	{ id: 0, name: "School Name", type: "text" },
+	{ id: 1, name: "Degree", type: "text" },
+	{ id: 3, name: "Field of Study", type: "text" },
+	{ id: 4, name: "Graduation Date", type: "date" },
+];
 
 export default function Education() {
-  return (
-    <div>Education</div>
-  )
+	const [fields, setFields] = useState(initialFields);
+
+	return (
+		<div>
+			<Form title={"Education"} fields={fields} />
+		</div>
+	);
 }
--- src/components/Professional.jsx
+++ src/components/Professional.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useState } from "react";
+import Form from "./Form";
+
+const initialFields = [
+	{ id: 0, name: "Company Name", type: "text" },
+	{ id: 1, name: "Position", type: "text" },
+	{ id: 3, name: "Main Responsibilities", type: "text" },
+	{ id: 4, name: "Start Date", type: "date" },
+	{ id: 5, name: "End Date", type: "date" },
+];
 
 export default function Professional() {
-  return (
-    <div>Professional</div>
-  )
+	const [fields, setFields] = useState(initialFields);
+
+	return (
+		<div>
+			<Form title={"Professional Experience"} fields={fields} />
+		</div>
+	);
 }
